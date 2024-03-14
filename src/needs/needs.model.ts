9cc1f7d37a2cfcd832bd34b8028203f83273d0a0
therealjustinbiele <justin.biele@gmail.com>
2024-03-13 20:11:26 -0700

    feat: update sams branch

--- src/needs/needs.model.ts
+++ src/needs/needs.model.ts
@@ -1,44 +1,7 @@
 import { model, Schema } from 'mongoose'
 import User from '../users/user.model'
 
-// const Service = new Schema({
-//     serviceType: String,
-//     timeStart: Date,
-//     timeEnd: Date,
-//     priceBid: Number
-// })
-
-const Goods = new Schema({
-    item: {
-        type: String,
-        require: true
-    },
-    quantity: {
-        type: Number,
-        require: true
-    },
-    condition: {
-        type: String,
-        require: true
-    }
-})
-
-// const needTypeSchema = new Schema({
-//     service: {
-//         type: Service,
-//         required: function () {
-//             return !this.goods
-//         }
-//     },
-//     goods: {
-//         type: Goods,
-//         required: function () {
-//             return !this.service
-//         }
-//     }
-// })
-
-const NeedsSchema = new Schema({
+const NeedSchema = new Schema({
     user: {
         type: User,
         require: true
@@ -55,16 +18,13 @@ const NeedsSchema = new Schema({
         type: Number,
         require: true
     },
-    goods: {
-        type: Goods,
+    quantity: {
+        type: Number,
         require: true
     },
-    exchangeNeeds: {
-        type: Boolean,
-        require: true
-    }
+
 }, {
     timestamps: true
 })
 
-export default model('Need', NeedsSchema)
+export default model('Need', NeedSchema)
