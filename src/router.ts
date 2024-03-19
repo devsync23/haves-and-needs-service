d1b6f9b6a0870401f0fcb69c7dc0074403b06eba
Andrewl9786 <134465957+Andrewl9786@users.noreply.github.com>
2024-03-18 20:17:32 -0700

    Revert "added have validation middleware"

--- src/router.ts
+++ src/router.ts
@@ -16,9 +16,9 @@ import {
 }
 from './have/have.controller'
 
-import { validateUserRequest, validateRegister } from './users/user.middleware'
+import { validateUserRequest } from './users/user.middleware'
+
 import { verifyToken } from './middleware/auth'
-import { validateHave } from './have/have.middleware'
 
 import {
   createNeed,
@@ -67,7 +67,7 @@ router.route('/needs/:id')
 // Have routes
 router.route('/api/haves')
   .get(getHaves)
-  .post(validateHave, createHave)
+  .post(createHave)
 
 router.route('/api/haves/:id')
   .get(getHave)
